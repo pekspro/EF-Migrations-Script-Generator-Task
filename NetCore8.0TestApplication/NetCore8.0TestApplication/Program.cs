e5cd0b01edb2ed8bf6af7f1990651ae3882bff4f
pekspro <msn@pekspro.com>
2023-11-19 07:46:40 +0100

    Adds test project for .NET 8.

---
+++ NetCore8.0TestApplication/NetCore8.0TestApplication/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using NetCore8TestApplication.Data;
+using NetCore8TestLibrary;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<FirstDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FirstDatabaseContext")));
+builder.Services.AddDbContext<SecondDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SecondDatabaseContext")));
+builder.Services.AddDbContext<LibraryDatabaseContext>(options =>
+   options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryDatabaseContext")));
+builder.Services.AddDbContext<InternalLibraryDatabaseContext>(options =>
+   options.UseSqlServer(builder.Configuration.GetConnectionString("InternalLibraryDatabaseContext")));
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
---
+++ NetCore8.0TestApplication/NetCore8.0TestLibrary/InternalLibraryDatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("NetCore8.0TestApplication")]
+
+namespace NetCore8TestLibrary;
+
+
+internal class InternalLibraryDatabaseContext : DbContext
+{
+    public InternalLibraryDatabaseContext(DbContextOptions<InternalLibraryDatabaseContext> options)
+            : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+
+    public DbSet<LibraryDatabaseDataModel> LibraryDatabaseData { get; set; } = null!;
+}
