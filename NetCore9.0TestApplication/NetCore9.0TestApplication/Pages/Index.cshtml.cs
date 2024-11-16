9cc1c6a44dd895a297d7115acde82da8a42eb937
pekspro <msn@pekspro.com>
2024-11-16 19:37:51 +0100

    Adds test application for .NET 9.

---
+++ NetCore9.0TestApplication/NetCore9.0TestApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using NetCore9TestApplication.Data;
+
+namespace NetCore9TestApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger, FirstDatabaseContext firstDatabaseContext, SecondDatabaseContext secondDatabaseContext)
+        {
+            _logger = logger;
+
+            FirstDatabaseContext = firstDatabaseContext;
+            SecondDatabaseContext = secondDatabaseContext;
+        }
+
+        public FirstDatabaseContext FirstDatabaseContext { get; }
+        public SecondDatabaseContext SecondDatabaseContext { get; }
+
+        public async Task OnGetAsync()
+        {
+            var firstData = await FirstDatabaseContext.FirstDataModels.ToListAsync();
+            var secondData = await SecondDatabaseContext.SecondDataModels.ToListAsync();
+        }
+    }
+}
---
+++ NetCore9.0TestApplication/NetCore9.0TestLibrary/InternalLibraryDatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("NetCore9.0TestApplication")]
+
+namespace NetCore9TestLibrary;
+
+
+internal class InternalLibraryDatabaseContext : DbContext
+{
+    public InternalLibraryDatabaseContext(DbContextOptions<InternalLibraryDatabaseContext> options)
+            : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+
+    public DbSet<LibraryDatabaseDataModel> LibraryDatabaseData { get; set; } = null!;
+}
